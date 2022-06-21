57c833e6a498631de3097f0eead6d0ed58f64164
Антон <043@list.ru>
2022-06-21 21:17:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,67 @@
-function getRandomIntInclusive(min, max) {
-    min = 20;
-    max = 50;
-    
-    if (max < min) {
-    console.log('Привет');
-    }else
-    
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
-  console.log(getRandomIntInclusive());
+const ID = [];
+const URL = [];
+const DESCRIPTION = ['описание один','описание два','описание три','описание четыре','описание пять'];
 
+const MESSAGE = [
+'Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+	]
+const NAME = ['Артём','Антон','Стас','Никита','Максим','Саша','Дима']
 
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
 
-  let checkLength = function (stroka, maxLength) {
+for (let i = 0; i < 25; i++) { // создаём массив ID
+  ID[i] = i + 1;
+}
+for (let i = 0; i < 25; i++) { // создаём массив URL (для фоток)
+  URL[i] = i + 1;
+}
+
+// делаем описание к фотке
+const createObjectDop = () => {
+  // делаем уникальность для комментария, склеиваем два разных сообщения
+  for (let i = 0; i < MESSAGE.length - 1; i++) {
+    randomMessageFirst = getRandomArrayElement(MESSAGE);
+    randomMessageSecond = getRandomArrayElement(MESSAGE);
+    if (randomMessageFirst != randomMessageSecond) {
+      randomMessage = randomMessageFirst + ' ' + randomMessageSecond;   
+      }
+    }
   
-  
-    if (stroka.length <= maxLength){
-    console.log('Длинна строки ' + '"' + stroka + '"' + ' = ' + stroka.length + ' и она меньше ' + maxLength + '. Всё норм!');
-      }else
-      console.log('Длинна строки ' + '"' + stroka + '"' + ' = ' + stroka.length + ' и она больше чем ' + maxLength + '. Сократите комментарий!');
+  return {
+    comment_id: getRandomPositiveInteger(1, 1000),        
+    comment_avatar: 'img/avatar-' + getRandomPositiveInteger(1, 6) + '.svg',
+    comment_message: randomMessage,
+    comment_name: getRandomArrayElement(NAME),
   };
-  checkLength('Кекс', 10);
+};
+const oneObjectDop = Array.from({length: 1}, createObjectDop);
+
+// создаём объект и добавляем к нему описание
+const createObject = () => {
   
-  
-  
-  //имя_функции(проверяемая_строка, максимальная_длина); 
-  // Результат: true, если строка проходит по длине, и false — если не проходит
+  return {
+    id: getRandomArrayElement(ID),
+    url: 'photos/' + getRandomArrayElement(URL) + '.jpg',
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomPositiveInteger(15, 200),
+    fotoDescription: oneObjectDop, // + описание к фото
+  };
+};
+
+
+const oneObject = Array.from({length: 1}, createObject); // пока что "1" для теста
+console.log(oneObject);
